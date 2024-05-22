ecd118799057dfecd04fc2857fbd85b7e8ca347f
Magnus Bolton <magnus@bolton.dk>
2024-05-22 16:36:05 +0200

    slideshow ny

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
 const links = document.querySelectorAll(".menu li a");
+const imageInn = document.querySelector("imageInn");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
